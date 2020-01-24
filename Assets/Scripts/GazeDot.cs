6578f7d5b861c6fe099c8a62e8c8235adb40e52f
Desktop_Supermachine <yourmum@hotmail.com>
2020-01-25 00:51:57 +0100

    Added gaze dot indicator

---
+++ Assets/Scripts/GazeDot.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.XR;
+using UnityEngine;
+
+public class GazeDot : MonoBehaviour
+{
+    // Max. distance of gaze ray
+    [SerializeField]
+    private float _gazeRange = 500f;
+
+    // How many frames (n) position of gaze dot is determined from
+    [SerializeField]
+    private int _numFramesToSample = 30;
+
+    private Transform _player;
+
+    // World space pos. of gaze dot last frame
+    private Vector3 _lastDotPos;
+
+    // Sampled world space gaze pos. for last n frames (stored as ring buffer)
+    private Vector3[] _sampledPoints;
+
+    // Index in sampled points of most recent sample (always mod n)
+    private int _frameIndex = 0;
+
+    private void Awake()
+    {
+        _player = Camera.main.transform;
+        _sampledPoints = new Vector3[_numFramesToSample];
+    }
+
+    private void Update()
+    {
+        Vector3 hitPos;
+
+        TobiiXR_GazeRay ray = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World).GazeRay;
+
+        if (Physics.Raycast(ray.Origin, ray.Direction, out RaycastHit hit))
+        {
+            hitPos = hit.point;
+        }
+        else
+        {
+            Debug.Log("Looking outside range");
+            hitPos = ray.Direction * _gazeRange;
+        }
+
+        _sampledPoints[_frameIndex] = hitPos;
+
+        float eyeVelocity = Vector3.Distance(_lastDotPos, hitPos) / Time.deltaTime;
+
+        Vector3 dotPos = Vector3.zero;
+        _frameIndex = (_frameIndex + 1) % _numFramesToSample;
+
+        int i = _frameIndex;
+        Debug.Assert(i >= 0 && i < _numFramesToSample, "Position average error: i is " + i);
+        int samplesUsed = 0;
+        do
+        {
+            dotPos += _sampledPoints[i];
+            i = (i + 1) % _numFramesToSample;
+            samplesUsed++;
+        }
+        while (samplesUsed < _numFramesToSample);
+        dotPos /= _numFramesToSample;
+        transform.position = dotPos - (0.1f * ray.Direction);
+        transform.rotation = Quaternion.LookRotation(_player.forward, _player.up);
+
+    }
+
+
+}
